1745f1e903ce4d84240ca9fc127245245c66a206
“mowatermelon <neefoxmo@gmail.com>
2018-01-04 23:53:37 +0800

    添加图片测试数据

--- js/draw.js
+++ js/draw.js
@@ -5,137 +5,171 @@ var viewerHeight='';
 function initData() {
   this.text ={
     "name": "西瓜",
+    "url":"img/twitter.png",
     "children": [
       {
         "name": "西瓜01",
+        "url":"img/twitter.png",
         "children": [
           {
             "name": "西瓜01_1",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "melon01_1",
+                "url":"img/twitter.png",
                 "size": 3938
               },
               {
                 "name": "melon01_2",
+                "url":"img/twitter.png",
                 "size": 3812
               },
               {
                 "name": "melon01_3",
+                "url":"img/twitter.png",
                 "size": 6714
               },
               {
                 "name": "melon01_4",
+                "url":"img/twitter.png",
                 "size": 743
               }
               ,
               {
                 "name": "water01_5",
+                "url":"img/twitter.png",
                 "size": 743
               },
               {
                 "name": "water01_6",
+                "url":"img/twitter.png",
                 "size": 743
               },
               {
                 "name": "water01_7",
+                "url":"img/twitter.png",
                 "size": 743
               },
               {
                 "name": "water01_8",
+                "url":"img/twitter.png",
                 "size": 743
               },
               {
                 "name": "water01_9",
+                "url":"img/twitter.png",
                 "size": 743
               }
             ]
           },
           {
             "name": "西瓜01_2",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "mo02_1",
+                "url":"img/twitter.png",
                 "size": 3534
               },
               {
                 "name": "mo02_2",
+                "url":"img/twitter.png",
                 "size": 5731
               },
               {
                 "name": "mo02_3",
+                "url":"img/twitter.png",
                 "size": 7840
               },
               {
                 "name": "mo02_4",
+                "url":"img/twitter.png",
                 "size": 5914
               },
               {
                 "name": "mo02_5",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_6",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_7",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_8",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_9",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_10",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_11",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_12",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_13",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_14",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_15",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_16",
+                "url":"img/twitter.png",
                 "size": 3416
               },
               {
                 "name": "mo02_17",
+                "url":"img/twitter.png",
                 "size": 3416
               }
             ]
           },
           {
             "name": "西瓜01_3",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "lucky03_1",
+                "url":"img/twitter.png",
                 "size": 7074
               },
               {
                 "name": "lucky03_2",
+                "url":"img/twitter.png",
                 "size": 7074
               },{
                 "name": "lucky03_3",
+                "url":"img/twitter.png",
                 "size": 7074
               }
             ]
@@ -144,112 +178,138 @@ function initData() {
       },
       {
         "name": "西瓜02",
+        "url":"img/twitter.png",
         "children": [  
           {
             "name": "西瓜02_1",
             "children": [
               {
                 "name": "lucky01_1",
+                "url":"img/twitter.png",
                 "size": 1983
               }
             ]
           },
            {
             "name": "西瓜02_2",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "lucky02_1",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky02_2",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky02_3",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky02_4",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky02_5",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky02_6",
+                "url":"img/twitter.png",
                 "size": 1983
               }
             ]
           },
            {
             "name": "西瓜02_3",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "water03_1",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "water03_2",
+                "url":"img/twitter.png",
                 "size": 1983
               }
             ]
           },
            {
             "name": "西瓜02_4",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "lucky04_1",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky04_2",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky04_3",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky04_4",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky04_5",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "lucky04_6",
+                "url":"img/twitter.png",
                 "size": 1983
               }
             ]
           },
            {
             "name": "西瓜02_5",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "mo05_1",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "mo05_2",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "mo05_3",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "mo05_4",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "mo05_5",
+                "url":"img/twitter.png",
                 "size": 1983
               },
               {
                 "name": "mo05_6",
+                "url":"img/twitter.png",
                 "size": 1983
               }
             ]
@@ -258,49 +318,61 @@ function initData() {
       },
       {
         "name": "西瓜03",
+        "url":"img/twitter.png",
         "children": [
           {
             "name": "西瓜03_1",
+            "url":"img/twitter.png",
             "children": [
               {
                 "name": "hhh01_1",
+                "url":"img/twitter.png",
                 "size": 721
               },
               {
                 "name": "hhh01_2",
+                "url":"img/twitter.png",
                 "size": 4294
               },
               {
                 "name": "hhh01_3",
+                "url":"img/twitter.png",
                 "size": 9800
               },
               {
                 "name": "hhh01_4",
+                "url":"img/twitter.png",
                 "size": 1314
               },
               {
                 "name": "hhh01_5",
+                "url":"img/twitter.png",
                 "size": 2220
               }
               ,
               {
                 "name": "hhh01_6",
+                "url":"img/twitter.png",
                 "size": 4294
               },
               {
                 "name": "hhh01_7",
+                "url":"img/twitter.png",
                 "size": 9800
               },
               {
                 "name": "hhh01_8",
+                "url":"img/twitter.png",
                 "size": 1314
               },
               {
                 "name": "hhh01_9",
+                "url":"img/twitter.png",
                 "size": 2220
               },
               {
                 "name": "hhh01_10",
+                "url":"img/twitter.png",
                 "size": 9800
               }
             ]
