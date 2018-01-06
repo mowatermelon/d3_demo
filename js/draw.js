76ec7424ecc3e5aabc60b761cedd9413e39cb681
“mowatermelon <neefoxmo@gmail.com>
2018-01-06 12:51:20 +0800

    添加数据量比较小的测试

--- js/draw.js
+++ js/draw.js
@@ -385,6 +385,98 @@ function initData() {
   }
 }
 
+function littleData(){
+  this.text ={
+    "name": "Maude Fernandez",
+    "id": "06ada7cd-3078-54bc-bb87-72e9d6f38abf",
+    "url":"img/icon.png",
+    "children": [
+      {
+        "name": "Janie Norton",
+        "id": "a39bfa73-6617-5e8e-9470-d26b68787e52",
+        "url":"img/icon.png",
+        "children": [
+          {
+            "name": "Pearl Cannon",
+            "id": "fc956046-a5c3-502f-b853-d669804d428f",
+            "url":"img/icon.png",
+            "children": [
+              {
+                "name": "Augusta Miller",
+                "id": "fa5b0c07-9000-5475-a90e-b76af7693a57",
+                "url":"img/icon.png"
+              },
+              {
+                "name": "Clayton Welch",
+                "id": "3194517d-1151-502e-a3b6-d1ae8234c647",
+                "url":"img/icon.png"
+              }
+            ]
+          },
+          {
+            "name": "Nell Morton",
+            "id": "06c7b0cb-cd21-53be-81bd-9b088af96904",
+            "url":"img/icon.png",
+            "children": [
+              {
+                "name": "Lelia Hernandez",
+                "id": "667d2bb6-c26e-5881-9bdc-7ac9805f96c2",
+                "url":"img/icon.png"
+              },
+              {
+                "name": "Randy Welch",
+                "id": "104039bb-d353-54a9-a4f2-09fda08b58bb",
+                "url":"img/icon.png"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Helen Alvarado",
+        "id": "522266d2-f01a-5ec0-9977-622e4cb054c0",
+        "url":"img/icon.png",
+        "children": [
+          {
+            "name": "Gussie Glover",
+            "id": "da430aa2-f438-51ed-ae47-2d9f76f8d831",
+            "url":"img/icon.png",
+            "children": [
+              {
+                "name": "Mina Freeman",
+                "id": "d384197e-2e1e-5fb2-987b-d90a5cdc3c15",
+                "url":"img/icon.png"
+              },
+              {
+                "name": "Charlotte Martin",
+                "id": "ea01728f-e542-53a6-acd0-6f43805c31a3",
+                "url":"img/icon.png"
+              }
+            ]
+          },
+          {
+            "name": "Jesus Pierce",
+            "id": "bfd1612c-b90d-5975-824c-49ecf62b3d5f",
+            "url":"img/icon.png",
+            "children": [
+              {
+                "name": "Donald Cox",
+                "id": "4f910be4-b827-50be-b783-6ba3249f6ebc",
+                "url":"img/icon.png"
+              },
+              {
+                "name": "Alex Gonzales",
+                "id": "efb2396d-478a-5cbc-b168-52e028452f3b",
+                "url":"img/icon.png"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+
 $(function () {
   // 图的大小
   viewerWidth = $(document).width();
